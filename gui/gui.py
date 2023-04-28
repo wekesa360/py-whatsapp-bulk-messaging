9b3b19f57274975095571109cc5c8824bc94bab9
Michael Wekesa <wekesa360@outlook.com>
2023-04-28 20:48:10 +0300

    add upload gui

---
+++ gui/gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import filedialog
+
+class Upload:
+    def __init__(self, parent):
+        self.parent = parent
+        self.file_path = None
+
+    def open_file_dialog(self):
+        self.file_path = filedialog.askopenfilename(initialdir="/", title="Select file",
+                                                    filetypes=[("CSV files", "*.csv")])
+        return self.file_path
+
