2b748dfd0a59e7b48ee7e42b2cf8b70863a9aca0
Michael Wekesa <wekesa360@outlook.com>
2023-04-28 19:29:07 +0300

    add whatsapp session

--- whatsapp_bot/whatsapp_bot.py
+++ whatsapp_bot/whatsapp_bot.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
 import threading
 
 class WhatsAppBot(threading.Thread):
@@ -14,7 +16,28 @@ class WhatsAppBot(threading.Thread):
         threading.Thread.__init__(self)
         self.phone_number = phon_number
         self.message = message
+        self.driver = self.get_driver()
     
+    def get_driver(self):
+        session_file_path = "session.data"
+        chrome_options = Options()
+        chrome_options.add_argument("user-data-dir=selenium")
+
+        if os.path.exists(session_file_path):
+            # If the session file exists, use it to authenticate
+            chrome_options.add_argument(f"session-id={session_file_path}")
+        
+        driver = webdriver.Chrome(options=chrome_options)
+
+        # Save the session file
+        if not os.path.exists(session_file_path):
+            # If the session file does not exist, authenticate and save the session
+            driver.get("https://web.whatsapp.com/")
+            WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@title='Menu']")))
+            driver.save_session(session_file_path)
+        
+        return driver
+
     def run(self):
         # Create a new instance of the web driver
         service = Service(ChromeDriverManager().install())
@@ -22,8 +45,8 @@ class WhatsAppBot(threading.Thread):
         # Go to the whatsapp web
         driver.get("https://web.whatsapp.com/")
 
-        # Wait for the user to scan the QR code
-        input("Press enter after scanning QR code")
+        #wait for chat to load
+        time.sleep(10)
 
         # Find contact in search box
         try:
