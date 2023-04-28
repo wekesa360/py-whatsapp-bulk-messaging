de0331ade9729bc6b4e11f6d20abb0b97e3072ff
Michael Wekesa <wekesa360@outlook.com>
2023-04-28 14:30:42 +0300

    refactor code

--- contact_converter/contact_converter.py
+++ contact_converter/contact_converter.py
@@ -1,6 +1,6 @@
 import csv
 import re
-import phonenumbers
+
 class ContactConverter:
     def __init__(self, filename, column_name):
         self.filename = filename
@@ -8,11 +8,12 @@ class ContactConverter:
     
     def convert(self):
         contacts = []
-
         with open(self.filename, 'r') as csv_file:
             reader = csv.DictReader(csv_file)
+            
             for row in reader:
                 if self.column_name in row:
+                    
                     contact = self.validate(row[self.column_name])
                     if contact:
                         contacts.append(contact)
@@ -20,19 +21,13 @@ class ContactConverter:
         return contacts
     
     def validate(self, contact):
+        # Convert the contact number to the international format
+        if contact.startswith(('7', '1')):
+            contact = '+254' + contact
+        elif contact.startswith('0'):
+            contact = '+254' + contact[1:]
         # Remove all non-numeric characters from the contact number
-        contact = ''.join([c for c in contact if c.isdigit()])
-
-        # Check if the contact number is valid
-        try:
-            parsed_number = phonenumbers.parse(contact, None)
-        except phonenumbers.NumberParseException:
-            return None
+        contact = re.sub(r'[^\d+]', '', contact)
         
-        # Check if the contact number is a mobile number
-        if not phonenumbers.is_valid_number(parsed_number):
-            return None
-        
-        # Format the contact number in the international format
-        return f'+{str(parsed_number.country_code)}{str(parsed_number.national_number)}'
+        return contact
         
