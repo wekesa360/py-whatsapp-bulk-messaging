85cd63c47225f5abd8a2213c2751cb76c04bea18
Michael Wekesa <wekesa360@outlook.com>
2023-05-01 14:05:58 +0300

    refactor, fix bug

--- whatsapp_bot/whatsapp_bot.py
+++ whatsapp_bot/whatsapp_bot.py
@@ -1,21 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-import pickle
-import os
 import threading
 
-class WhatsAppBot(threading.Thread):
-    def __init__(self, phon_number, message):
+class WhatsAppBot(threading.Thread):    
+    def __init__(self, phone_numbers, message):
         threading.Thread.__init__(self)
-        self.phone_number = phon_number
+        self.phone_numbers = phone_numbers
         self.message = message
         self.driver = None
     
@@ -24,76 +20,55 @@ class WhatsAppBot(threading.Thread):
         if self.driver:
             return self.driver
 
-        # Load the session from the file if it exists
-        session_file_path = "whatsapp_session.pkl"
-        try:
-            with open(session_file_path, "rb") as f:
-                session = pickle.load(f)
-            self.driver = webdriver.Chrome(executable_path=self.chrome_driver_path)
-            self.driver.session_id = session["session_id"]
-            self.driver.command_executor._url = session["executor_url"]
-            return self.driver
-        except FileNotFoundError:
-            pass
-
-        # If session file does not exist, create a new driver instance
         options = webdriver.ChromeOptions()
         options.add_argument("--user-data-dir=chrome-data")
         options.add_argument("--profile-directory=Default")
-        self.driver = webdriver.Chrome(executable_path=self.chrome_driver_path, options=options)
+        # Create a new instance of the web driver
+        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
         self.driver.get("https://web.whatsapp.com/")
         
-        # Save the session to a file
-        session = {
-            "session_id": self.driver.session_id,
-            "executor_url": self.driver.command_executor._url,
-        }
-        with open(session_file_path, "wb") as f:
-            pickle.dump(session, f)
-
         return self.driver
-
-    def run(self):
-        # Create a new instance of the web driver
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
-        # Go to the whatsapp web
-        driver.get("https://web.whatsapp.com/")
-
-        #wait for chat to load
-        time.sleep(10)
-
-        # Find contact in search box
+    
+    def search_number(self, driver, phone_number):
         try:
             search_box = WebDriverWait(driver, 50).until(
                 EC.presence_of_element_located((By.XPATH, '//*[@id="side"]/div[1]/div/div/div[2]/div/div[1]/p'))
             )
-            search_box.send_keys(self.phone_number)
+            search_box.send_keys(phone_number)
             search_box.send_keys(Keys.RETURN)
+            return None
         except TimeoutException:
             return "Timed out waiting for search box element to appear"
+        
+    def send_message(self, driver):
+        message_box = WebDriverWait(driver, 30).until(
+                        EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'))
+                    )
+        message_box.send_keys(self.message)
+        message_box.send_keys(Keys.RETURN)
+        return None
+
+    def run(self):
+        # Go to the whatsapp web
+        driver = self.get_driver()
 
         #wait for chat to load
-        time.sleep(5)
-
-        #Enter message in message box
-        try:
-            message_box = WebDriverWait(driver, 50).until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'))
-            )
-            message_box.send_keys(self.message)
-            message_box.send_keys(Keys.RETURN)
-        except TimeoutException:
-            print("Timed out waiting for search box element to appear")
-
-        #wait for message to be sent
-        time.sleep(2)
-
+        time.sleep(7)
+        for phone_number in self.phone_numbers:
+            # Find contact in search box
+            self.search_number(driver,phone_number)
+            #wait for chat to load
+            time.sleep(3)
+            #Enter message in message box
+            try:
+                self.send_message(driver)
+            except TimeoutException:
+                # Phone number is not saved as a contact, use wa.me link to send message
+                phone_number = phone_number[0:]
+                wa_link = f"https://web.whatsapp.com/send/?phone=%{phone_number}&text&type=phone_number&app_absent=0"
+                driver.get(wa_link)
+                self.send_message(driver)
+            #wait for message to be sent
+            time.sleep(2)
         #close the web driver
         driver.quit()
-
-if __name__ == "__main__":
-    # Create a new instance of the whatsapp bot
-    whatsapp_bot = WhatsAppBot(["+254792589417"], "Hello world")
-    whatsapp_bot.start()
-    whatsapp_bot.join()
